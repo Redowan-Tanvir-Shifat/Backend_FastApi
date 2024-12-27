6cf4eaf3c38f7d1d66f776b66452c863e619cc96
Redowan0019 <146367158+Redowan0019@users.noreply.github.com>
2024-12-27 20:44:17 +0600

    Get, Post, Put, Delete method (Complete)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query, HTTPException, status
 from typing import Optional
 from pydantic import BaseModel
 
@@ -7,6 +7,11 @@ app = FastAPI()
 class Item(BaseModel):
     name: str
     price: float
+    brand: Optional[str] = None
+
+class UpdateItem(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
     brand: Optional[str] = None
 
 inventory = {}
@@ -32,7 +37,7 @@ def get_item(name: str = None):
     for item_id in inventory:
         if inventory[item_id].name == name:
             return inventory[item_id]
-    return {"Data": "Not Found"}
+    raise HTTPException(status_code=404, detail="Item name not found")
 
 # # <-----Query Parameters----->
 # @app.get("/get-by-name")
@@ -56,7 +61,34 @@ def get_item(name: str = None):
 @app.post("/create-item/{item_id}")
 def create_ites(item_id: int, item: Item):
     if item_id in inventory:
-        return {"Error": "Item ID already exists"}
+        raise HTTPException(status_code=400, detail="Item ID already exists")
     
     inventory[item_id] = item
     return inventory[item_id]
+
+
+
+# <--------------------------------------Put Request------------------------------------>
+@app.put("/update-item/{item_id}")
+def update_item(item_id: int, item: UpdateItem):
+    if item_id not in inventory:
+        raise HTTPException(status_code=404, detail="Item ID does not exist")
+    
+    if item.name != None:
+        inventory[item_id].name = item.name
+    if item.price != None:
+        inventory[item_id].price = item.price
+    if item.brand != None:
+        inventory[item_id].brand = item.brand
+    return inventory[item_id]
+
+
+
+# <--------------------------------------Delete Request------------------------------------>
+@app.delete("/delete-item")
+def delete_item(item_id: int = Query(..., description="The ID of the item to delete", gt=0)):
+    if item_id not in inventory:
+        raise HTTPException(status_code=404, detail="Item ID does not exist")
+    
+    del inventory[item_id]
+    return {"Message": "Item Deleted Successfully"}
